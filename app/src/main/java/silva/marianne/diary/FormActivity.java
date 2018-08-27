43a181ae7afb875edd7365fe2b0960e56de8fbef
Marianne Silva <marikell_silva@hotmail.com>
2018-08-27 17:13:18 -0300

    on click button

--- app/src/main/java/silva/marianne/diary/FormActivity.java
+++ app/src/main/java/silva/marianne/diary/FormActivity.java
@@ -2,6 +2,9 @@ package silva.marianne.diary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        Button saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormActivity.this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
